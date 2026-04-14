2f628fd87876d005f228d20e3e0ebbf76313c4b2
arnau <arnaud.dhaene@gmail.com>
2026-04-14 10:12:40 +0100

    QuestionsMarks

---
+++ src/main/java/com/bezkoder/spring/jpa/h2/exercices/QuestionsMarks.java
@@ -0,0 +1,63 @@
+package com.bezkoder.spring.jpa.h2.exercices;
+
+/*
+https://coderbyte.com/editor/Questions%20Marks:Java
+ */
+public class QuestionsMarks {
+
+    public static void main (String[] args) {
+
+        System.out.println(run("aa6?9"));//false
+        System.out.println(run("acc?7??sss?3rr1??????5"));//true
+    }
+
+    public static String run(String input) {
+        int i = 0;
+        while(i < input.length()) {
+            if(Character.isDigit(input.charAt(i))) {
+                int j = i + 1;
+                int qCount = 0;
+                while(j < input.length()) {
+                    if(input.charAt(j) == '?') qCount++;
+                    if(Character.isDigit(input.charAt(j))) {
+                        int sum = Character.getNumericValue(input.charAt(i)) + Character.getNumericValue(input.charAt(j));
+                        if((sum == 10 && qCount == 3)){
+                            return "true";
+                        }
+                        i = j;
+                        break;
+                    }
+                    j++;
+                }
+            }
+            i++;
+        }
+        return "false";
+//        char[] arrayStr = input.toCharArray();
+//        int index1 = 0;
+//        int index2 = 1;
+//        boolean found = false;
+//        for(int i = 0;i<arrayStr.length;i++) {
+//            if(found){
+//                break;
+//            }
+//            if(Character.isDigit(arrayStr[i])){
+//                index1 =i;
+//                for(int j = i+1;j<arrayStr.length;j++){
+//                    if (Character.isDigit(arrayStr[j])) {
+//                        index2 =j;
+//                        if(Character.getNumericValue(arrayStr[i]) + Character.getNumericValue(arrayStr[j]) == 10){
+//                            found = true;
+//                            break;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//        if(found){
+//            System.out.println(index1  + " " + index2);
+//        }
+//        return "true";
+    }
+}
